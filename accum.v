31b2818becc502cda2edc0597903e377a7e2336d
jiangbw210 <57691075+jiangbw210@users.noreply.github.com>
2019-11-14 21:48:18 -0600

    Add files via upload

---
+++ accum.v
@@ -0,0 +1,15 @@
+module accum (CLR, D, Q); 
+input  CLR; 
+input  [15:0] D; 
+output [15:0] Q; 
+reg    [15:0] tmp;  
+ 
+  always @(posedge CLR) 
+    begin 
+      if (CLR) 
+        tmp = 16'b0000000000000000; 
+      else 
+        tmp = tmp + D; 
+    end 
+  assign Q = tmp; 
+endmodule 
