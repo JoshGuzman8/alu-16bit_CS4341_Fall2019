0f7031afe93e7d0f4bf6671217527da8dfb12df0
ssMD16 <57784426+ssMD16@users.noreply.github.com>
2019-11-15 18:55:06 -0600

    Update adder-subtractor.v

--- adder-subtractor.v
+++ adder-subtractor.v
@@ -1,10 +1,10 @@
 module ripple_carry_adder_subtractor(S, C, V, A, B, Op);
-   output [15:0] S;   // The 16-bit sum/difference.
-   output 	C;   // The 1-bit carry/borrow status.
-   output 	V;   // The 1-bit overflow status.
+	output [15:0] Sum;   // The 16-bit sum/difference.
+   output 	Carry;   // The 1-bit carry/borrow status.
+   output 	Overflow;   // The 1-bit overflow status.
    input [15:0] 	A;   // The 16-bit augend/minuend.
    input [15:0] 	B;   // The 16-bit addend/subtrahend.
-   input 	Op;  // The operation: 0 => Add, 1=>Subtract.
+   input 	sel;  // The operation: 0 => Add, 1=>Subtract.
    
    wire 	C0; // The carry out bit of fa0, the carry in bit of fa1.
    wire 	C1; // The carry out bit of fa1, the carry in bit of fa2.
@@ -52,41 +52,41 @@ module ripple_carry_adder_subtractor(S, C, V, A, B, Op);
    // A+B when Op==0, and A+not(B)+1 when Op==1.
    // Note that not(B)+1 is the 2's complement of B, so
    // this gives us subtraction.     
-   	xor(B0, B[0], Op);
-   	xor(B1, B[1], Op);
-   	xor(B2, B[2], Op);
-   	xor(B3, B[3], Op);
-   	xor(B4, B[4], Op);
-   	xor(B5, B[5], Op);
-   	xor(B6, B[6], Op);
-   	xor(B7, B[7], Op);
-   	xor(B8, B[8], Op);
-   	xor(B9, B[9], Op);
-   	xor(B10, B[10], Op);
-	xor(B11, B[11], Op);  
-	xor(B12, B[12], Op);
-	xor(B13, B[13], Op);
-	xor(B14, B[14], Op);
-	xor(B15, B[15], Op);
-  	xor(C, C15, Op);     // Carry = C3 for addition, Carry = not(C3) for subtraction.
-   	xor(V, C15, C14);     // If the two most significant carry output bits differ, then we have an overflow.
+	xor(B0, B[0], sel);
+   	xor(B1, B[1], sel);
+   	xor(B2, B[2], sel);
+   	xor(B3, B[3], sel);
+   	xor(B4, B[4], sel);
+   	xor(B5, B[5], sel);
+   	xor(B6, B[6], sel);
+   	xor(B7, B[7], sel);
+   	xor(B8, B[8], sel);
+   	xor(B9, B[9], sel);
+   	xor(B10, B[10], sel);
+	xor(B11, B[11], sel);  
+	xor(B12, B[12], sel);
+	xor(B13, B[13], sel);
+	xor(B14, B[14], sel);
+	xor(B15, B[15], sel);
+	xor(Carry, C15, sel);     // Carry = C3 for addition, Carry = not(C3) for subtraction.
+	xor(Overflow, C15, C14);     // If the two most significant carry output bits differ, then we have an overflow.
    
-   full_adder fa0(S[0], C0, A[0], B0, Op);    // Least significant bit.
-   full_adder fa1(S[1], C1, A[1], B1, C0);
-   full_adder fa2(S[2], C2, A[2], B2, C1);
-   full_adder fa3(S[3], C3, A[3], B3, C2);    
-   full_adder fa4(S[4], C4, A[4], B4, C3);   
-   full_adder fa5(S[5], C5, A[5], B5, C4);
-   full_adder fa6(S[6], C6, A[6], B6, C5);
-   full_adder fa7(S[7], C7, A[7], B7, C6);    
-   full_adder fa8(S[8], C8, A[8], B8, C7);    
-   full_adder fa9(S[9], C9, A[9], B9, C8);
-   full_adder fa10(S[10], C10, A[10], B10, C9);
-   full_adder fa11(S[11], C11, A[11], B11, C10);   
-   full_adder fa11(S[12], C12, A[12], B12, C11);    
-   full_adder fa11(S[13], C13, A[13], B13, C12);    
-   full_adder fa11(S[14], C14, A[14], B14, C13);    
-   full_adder fa11(S[15], C15, A[15], B15, C15);    // Most significant bit.
+	full_adder fa0(Sum[0], C0, A[0], B0, sel);    // Least significant bit.
+   full_adder fa1(Sum[1], C1, A[1], B1, C0);
+   full_adder fa2(Sum[2], C2, A[2], B2, C1);
+   full_adder fa3(Sum[3], C3, A[3], B3, C2);    
+   full_adder fa4(Sum[4], C4, A[4], B4, C3);   
+   full_adder fa5(Sum[5], C5, A[5], B5, C4);
+   full_adder fa6(Sum[6], C6, A[6], B6, C5);
+   full_adder fa7(Sum[7], C7, A[7], B7, C6);    
+   full_adder fa8(Sum[8], C8, A[8], B8, C7);    
+   full_adder fa9(Sum[9], C9, A[9], B9, C8);
+   full_adder fa10(Sum[10], C10, A[10], B10, C9);
+   full_adder fa12(Sum[11], C11, A[11], B11, C10);   
+   full_adder fa13(Sum[12], C12, A[12], B12, C11);    
+   full_adder fa14(Sum[13], C13, A[13], B13, C12);    
+   full_adder fa15(Sum[14], C14, A[14], B14, C13);    
+   full_adder fa16(Sum[15], C15, A[15], B15, C15);    // Most significant bit.
 endmodule // ripple_carry_adder_subtractor
 
 
