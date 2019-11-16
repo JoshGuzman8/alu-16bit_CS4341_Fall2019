0388d33a857173cdccb1d0168e3de1f8116ff5c6
Albey Kappil <50112186+albkappil@users.noreply.github.com>
2019-11-15 21:00:23 -0600

    Update Opcode_Decoder.v

--- Opcode_Decoder.v
+++ Opcode_Decoder.v
@@ -1,15 +1,15 @@
-`define opAND  12'b000000000001
-`define opOR  12'b000000000010
-`define opNOT 12'b000000000100
-`define opXOR 12'b000000001000
-`define opNAND 12'b000000010000
-`define opNOR 12'b000000100000
-`define opCLEAR 12'b100000000000
-`define opXNOR 12'b000001000000
-`define opADD 12'b000010000000
-`define opSUB 12'b000100000000
-`define opSHRIGHT 12'b001000000000
-`define opSHLEFT 12'b010000000000
+`define selAND  12'b000000000001
+`define selOR  12'b000000000010
+`define selNOT 12'b000000000100
+`define selXOR 12'b000000001000
+`define selNAND 12'b000000010000
+`define selNOR 12'b000000100000
+`define selCLEAR 12'b100000000000
+`define selXNOR 12'b000001000000
+`define selADD 12'b000010000000
+`define selSUB 12'b000100000000
+`define selSHRIGHT 12'b001000000000
+`define selSHLEFT 12'b010000000000
 
 
 module decoder (sel, res);  
@@ -20,18 +20,18 @@ module decoder (sel, res);
  
   begin  
     case (sel)  
-      4'b0000 : res = opAND;  
-      4'b0001 : res = opOR ;  
-      4'b0010 : res = opNOT;  
-      4'b0011 : res = opXOR ;  
-      4'b0100 : res = opNAND;  
-      4'b0101 : res = opNOR;  
-      4'b0110 : res = opXNOR; 
-	  4'b1000 : res = opADD; 
-	  4'b1001 : res = opSUB;
-	  4'b1010 : res = opSHRIGHT;
-	  4'b1011 : res = opSHLEFT;
-      default : res = opCLEAR;  
+      4'b0000 : res = selAND;  
+      4'b0001 : res = selOR ;  
+      4'b0010 : res = selNOT;  
+      4'b0011 : res = selXOR ;  
+      4'b0100 : res = selNAND;  
+      4'b0101 : res = selNOR;  
+      4'b0110 : res = selXNOR; 
+	  4'b1000 : res = selADD; 
+	  4'b1001 : res = selSUB;
+	  4'b1010 : res = selSHRIGHT;
+	  4'b1011 : res = selSHLEFT;
+      4'b0000 : res = selCLEAR;  
     endcase  
   end  
 endmodule 
