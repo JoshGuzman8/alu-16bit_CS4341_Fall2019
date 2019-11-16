ef5611f4dc9c827f14082dd289eecd1c40d15c13
jiangbw210 <57691075+jiangbw210@users.noreply.github.com>
2019-11-15 18:59:16 -0600

    Update and_nand_not.v

--- and_nand_not.v
+++ and_nand_not.v
@@ -1,14 +1,16 @@
-module and_gate (a, b, c, y);
+module and_gate (a, b,c, y);
 input [15:0] a, b;
 input c;
-output [15:0] y;
-
-  always @(c or a or b)
- if(c == 0)
+output reg [15:0]y;
+always@(c)
+if (c == 0)
 assign y = a & b;
-  else
+else
 assign y = ~(a & b);
+
 endmodule
+
+
 
 module not_gate (a, y);
 input [15:0] a, b;
