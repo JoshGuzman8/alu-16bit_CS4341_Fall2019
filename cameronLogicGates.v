467ed52f74be8a93f3e90ca1dcfe0847fc200dd8
Cameron Buchman <cambuchman@comcast.net>
2019-11-15 19:09:41 -0600

    Update cameronLogicGates.v

--- cameronLogicGates.v
+++ cameronLogicGates.v
@@ -11,23 +11,14 @@ module orMod(a,b,or_output,nor_output)
         
 endmodule
 
-// FINISHED
-module xor_gate (a, b, c); 
- input [15:0] a, b; 
- output c; 
- wire c, a_not, b_not; 
-   not a_inv (a_not, a); 
-   not b_inv (b_not, b); 
-   and a1 (x, a_not, b); 
-   and a2 (y, b_not, a); 
-   or out (c, x, y); 
-endmodule 
+// This will return the xor and xnor output
+module xorMod(a,b,xor_output,xnor_output)
+    input [15:0] a, b;
+    output [15:0] xor_output, xnor_output;
 
-// FINISHED
-module xnor_gate(X,A, B);
-   output X;
-   input [15:0]  A;
-   input [15:0]  B;
+    always(@posedg clk)
 
-   xnor(X,A, B);
-endmodule
+    assign xor_output = ^(a | b);
+    assign xnor_output = ^~(a | b);
+
+end module
