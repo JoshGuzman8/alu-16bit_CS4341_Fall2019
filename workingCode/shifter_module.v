79974a79bea6fe4c63bf843fe517523fbcf51037
JoshGuzman8 <47124181+JoshGuzman8@users.noreply.github.com>
2019-11-17 06:58:01 -0600

    Update shifter_module.v

--- workingCode/shifter_module.v
+++ workingCode/shifter_module.v
@@ -1,16 +1,23 @@
 //=============================================
-// shifter module
+//  shifter module
+//  Author: Josh Guzman
+//
+//  Description: 
+//      This module takes in a 16-bit number
+//      and does a left and right shift of the
+//      the number on the positive edge of the
+//      clock.
 //=============================================
 module shifter(a, clk, leftShift, rightShift);
     //---------------------------------------------
     //Inputs/Outputs/regs
     //---------------------------------------------
-        input [15:0] a;
-        input clk;
-        output [15:0] leftShift;
-        output [15:0] rightShift;
+        input [15:0] a;             //16-bit number
+        input clk;                  //1-bit clock
+        output [15:0] leftShift;    //a shifted to left
+        output [15:0] rightShift;   //a shifted to right
     
-        reg [15:0] reg_leftShift, reg_rightShift;
+        reg [15:0] reg_leftShift, reg_rightShift;   //reg to hold primatives to pass to wire
 
     always @(posedge clk) begin
         reg_leftShift = a << 1;
