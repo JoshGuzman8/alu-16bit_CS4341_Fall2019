db85811236670b3b130a7e4021ca4c51e5731544
jiangbw210 <57691075+jiangbw210@users.noreply.github.com>
2019-11-22 15:44:23 -0600

    Update AND_NAND_modules.v

--- workingCode/AND_NAND_modules.v
+++ workingCode/AND_NAND_modules.v
@@ -1,4 +1,15 @@
 //=============================================
+//  And Nand module
+//  Author: Bowen Jiang
+//
+//  Description: 
+//      This module takes in two 16-bit number
+//      for input and make the 'and' and 'nand' 
+//      result of it to be a 16-bit 
+//      output on the positive edge of the
+//      clock.
+//=============================================
+//=============================================
 // AND module
 //=============================================
 module and_gate (a, b, clk, andout);
