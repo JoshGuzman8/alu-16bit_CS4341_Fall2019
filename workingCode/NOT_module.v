d651a3944338d47bdf210c67b8bc5dfe1fc41209
jiangbw210 <57691075+jiangbw210@users.noreply.github.com>
2019-11-22 15:42:00 -0600

    Update NOT_module.v

--- workingCode/NOT_module.v
+++ workingCode/NOT_module.v
@@ -1,11 +1,21 @@
 //=============================================
-// OR module
+//  accumulator module
+//  Author: Bowen Jiang
+//
+//  Description: 
+//      This module takes in a 16-bit number
+//      for input and make the swap of it to be a 16-bit 
+//      output on the positive edge of the
+//      clock.
+//=============================================
+//=============================================
+// Not module
 //=============================================
 module not_gate (a,clk, notout);
     //---------------------------------------------
     //Inputs/Outputs/regs
     //---------------------------------------------
-    input [15:0] a, b;
+    input [15:0] a;
     input clk;
     output [15:0] notout;
   
