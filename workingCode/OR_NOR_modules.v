8cecc2a71104629a49cef0c1937fbb478f685ddc
Cameron Buchman <cambuchman@comcast.net>
2019-11-22 18:00:26 -0600

    Update OR_NOR_modules.v

--- workingCode/OR_NOR_modules.v
+++ workingCode/OR_NOR_modules.v
@@ -1,12 +1,17 @@
 //=============================================
-// OR module
+//  OR module
+//  Author: Cameron Buchman
+//
+//  Description: 
+//      This module takes in two 16-bit numbers
+//      and will return one 16-bit number, either
+//      A or B as long as at least one is valid.
 //=============================================
-module orMod(a, b, clk, or_output);
+module orMod(a, b, or_output);
     //---------------------------------------------
     //Inputs/Outputs/regs
     //---------------------------------------------
         input [15:0] a, b;
-        input clk;
         output [15:0] or_output;
         reg [15:0] reg_or_output;
 
@@ -19,14 +24,19 @@ module orMod(a, b, clk, or_output);
 endmodule
 
 //=============================================
-// NOR module
+//  NOR module
+//  Author: Cameron Buchman
+//
+//  Description: 
+//      This module takes in two 16-bit numbers
+//      and will return one 16-bit number, as long
+//      as neither A or B are valid.
 //=============================================
-module norMod(a, b, clk, nor_output);
+module norMod(a, b, nor_output);
     //---------------------------------------------
     //Inputs/Outputs/regs
     //---------------------------------------------
         input [15:0] a, b;
-        input clk;
         output [15:0] nor_output;
         reg [15:0] reg_or_output, reg_nor_output;
 
