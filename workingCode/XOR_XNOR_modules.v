46ec2a016676aafc75e11e53acaf45d06f17d08f
JoshGuzman8 <47124181+JoshGuzman8@users.noreply.github.com>
2019-11-22 18:02:05 -0600

    Update XOR_XNOR_modules.v

--- workingCode/XOR_XNOR_modules.v
+++ workingCode/XOR_XNOR_modules.v
@@ -2,12 +2,11 @@
 // XOR module
 //=============================================
 // This will return the xor and xnor output
-module xorMod(a, b, clk, xor_output);
+module xorMod(a, b, xor_output);
     //---------------------------------------------
     //Inputs/Outputs/regs
     //---------------------------------------------
         input [15:0] a, b;
-        input clk;
         output [15:0] xor_output;
         
         reg [15:0] reg_xor_output;
@@ -24,12 +23,11 @@ endmodule
 // XNOR module
 //=============================================
 // This will return the xor and xnor output
-module xnorMod(a, b, clk, xnor_output);
+module xnorMod(a, b, xnor_output);
     //---------------------------------------------
     //Inputs/Outputs/regs
     //---------------------------------------------
         input [15:0] a, b;
-        input clk;
         output [15:0] xnor_output;
         
         reg [15:0] reg_xnor_output;
